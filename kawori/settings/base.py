d3d55d9665557fbc43ca4c0ba52d9698637128b8
Guilherme Henrique <ghpm99@hotmail.com>
2022-06-19 15:37:38 -0300

    fix: remove static dir

--- kawori/settings/base.py
+++ kawori/settings/base.py
@@ -119,8 +119,5 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT, '../../apps'))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'kawori/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
