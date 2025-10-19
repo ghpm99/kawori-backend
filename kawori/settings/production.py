b2e6e2b4e55cc9e143a4434d47364feaab339124
ghpm99 <ghpm99@gmail.com>
2025-10-18 23:03:14 -0300

    fix: production variables

--- kawori/settings/production.py
+++ kawori/settings/production.py
@@ -7,33 +7,36 @@ from kawori.settings.base import *  # noqa: F403, F401
 DEBUG = False
 
 sentry_sdk.init(
-    dsn=os.environ.get('SENTRY_DNS'),
-
+    dsn=os.environ.get("SENTRY_DNS"),
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    traces_sample_rate=1.0
+    traces_sample_rate=1.0,
 )
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('NAME'),
-        'USER': os.environ.get('USER'),
-        'PASSWORD': os.environ.get('PASSWORD'),
-        'HOST': os.environ.get('HOST')
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("NAME"),
+        "USER": os.environ.get("USER"),
+        "PASSWORD": os.environ.get("PASSWORD"),
+        "HOST": os.environ.get("HOST"),
     }
 }
 
-CORS_ALLOW_ORIGINS = [
-    "https://www.kawori.site/"
-]
+CORS_ALLOW_ORIGINS = ["https://www.kawori.site/"]
+
+BASE_URL_FRONTEND = "https://www.kawori.site"
+
+BASE_URL_FRONTEND_FINANCIAL = "https://financeiro.kawori.site"
+
+BASE_URL_FRONTEND_LIST = [BASE_URL_FRONTEND, BASE_URL_FRONTEND_FINANCIAL]
 
 COOKIE_DOMAIN = ".kawori.site"
 
